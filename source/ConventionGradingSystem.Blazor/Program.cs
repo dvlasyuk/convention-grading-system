33507ba4fe8c18b5ff96db0e8bc40113a5db361f
Dmitriy Vlasyuk <dmitriyvlasyuk@yandex.ru>
2023-10-18 16:21:48 +0300

    Заготовка проекта для реализации приложения с помощью Blazor

---
+++ source/ConventionGradingSystem.Blazor/Program.cs
@@ -0,0 +1,39 @@
+using ConventionGradingSystem.Blazor.Configuration;
+using ConventionGradingSystem.DataAccess;
+using ConventionGradingSystem.DataAccess.Database;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace ConventionGradingSystem.Blazor;
+
+/// <summary>
+/// Основной класс приложения, содержащий точку входа.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// Основной метод приложения, являющийся точкой входа.
+    /// </summary>
+    /// <param name="arguments">Аргументы командной строки, переданные приложению.</param>
+    public static void Main(string[] arguments)
+    {
+        var applicationBuilder = WebApplication.CreateBuilder(arguments);
+
+        applicationBuilder.Services.AddOptions<SecurityConfiguration>().BindConfiguration("SecurityConfiguration");
+        applicationBuilder.Services.AddSingleton<IValidateOptions<SecurityConfiguration>, SecurityConfigurationValidator>();
+
+        applicationBuilder.Services.AddDataAccess(
+            configurationSection: "ApplicationConfiguration",
+            connectionString: "ApplicationDatabase");
+
+        var application = applicationBuilder.Build();
+
+        using var migrationScope = application.Services.CreateScope();
+        var databaseContext = migrationScope.ServiceProvider.GetRequiredService<DatabaseContext>();
+        databaseContext.Database.Migrate();
+
+        application.MapGet("/", () => "Hello World!");
+        application.Run();
+    }
+}
